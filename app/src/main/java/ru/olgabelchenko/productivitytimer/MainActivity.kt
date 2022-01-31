2183101c4a772d8fce2244ce216b12ad32b97872
Olga Belchenko <>
2022-01-31 16:18:18 +0500

    Added stopwatch functionality

--- app/src/main/java/ru/olgabelchenko/productivitytimer/MainActivity.kt
+++ app/src/main/java/ru/olgabelchenko/productivitytimer/MainActivity.kt
@@ -2,10 +2,64 @@ package ru.olgabelchenko.productivitytimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import ru.olgabelchenko.productivitytimer.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var updateTime: Runnable
+    private val handler = Handler(Looper.getMainLooper())
+    private var isTimerRunning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding) {
+
+            Runnable {
+                val currentText = textView.text
+
+                var (minutes, seconds) = currentText.split(":").map { it.toInt() }
+
+                if (seconds < 59) {
+                    seconds++
+                } else {
+                    minutes++
+                    seconds = 0
+                }
+
+                textView.text = String.format("%02d:%02d", minutes, seconds)
+            }.also { updateTime = it }
+
+            startButton.setOnClickListener {
+
+                if (!isTimerRunning) {
+                    isTimerRunning = true
+                    thread {
+                        while (isTimerRunning) {
+                            handler.post(updateTime)
+                            Thread.sleep(1000)
+                        }
+                    }
+                }
+            }
+
+            resetButton.setOnClickListener {
+                isTimerRunning = false
+                textView.text = "00:00"
+            }
+
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        handler.removeCallbacks(updateTime)
     }
 }
