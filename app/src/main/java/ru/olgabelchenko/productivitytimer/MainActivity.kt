9db143cedc4da3b9e17a4bb0cf1cee704d3f18b1
Olga Belchenko <>
2022-02-15 12:02:32 +0500

    Added upper time limit alert dialog

--- app/src/main/java/ru/olgabelchenko/productivitytimer/MainActivity.kt
+++ app/src/main/java/ru/olgabelchenko/productivitytimer/MainActivity.kt
@@ -1,11 +1,14 @@
 package ru.olgabelchenko.productivitytimer
 
+import android.app.AlertDialog
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import ru.olgabelchenko.productivitytimer.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
@@ -15,8 +18,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var updateTime: Runnable
     private lateinit var changeProgressBarColor: Runnable
+    private lateinit var changeTextColor: Runnable
     private val handler = Handler(Looper.getMainLooper())
     private var time = 0
+    private var upperLimit = 0
 
     private var colorIndex = 0
     private val colorList = listOf(Color.GREEN, Color.BLUE, Color.YELLOW, Color.MAGENTA)
@@ -27,6 +32,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         with(binding) {
+
+            val initialColor = textView.textColors
 
             updateTime = object : Runnable {
                 override fun run() {
@@ -46,12 +53,23 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+            changeTextColor = object  : Runnable {
+                override fun run() {
+                    if (upperLimit != 0 && time > upperLimit) {
+                        textView.setTextColor(Color.RED)
+                    }
+                    handler.postDelayed(this, 1000)
+                }
+            }
+
             startButton.setOnClickListener {
                 progressBar.visibility = View.VISIBLE
+                settingsButton.isEnabled = false
                 if (time == 0) {
                     thread {
                         handler.postDelayed(updateTime, 1000)
                         handler.postDelayed(changeProgressBarColor, 1000)
+                        handler.postDelayed(changeTextColor, 1000)
                     }
                 }
             }
@@ -61,8 +79,23 @@ class MainActivity : AppCompatActivity() {
                 handler.removeCallbacks(changeProgressBarColor)
                 time = 0
                 colorIndex = 0
+                textView.setTextColor(initialColor)
                 progressBar.visibility = View.INVISIBLE
+                settingsButton.isEnabled = true
                 textView.text = "00:00"
+            }
+
+            settingsButton.setOnClickListener {
+                val contentView = LayoutInflater.from(this@MainActivity).inflate(R.layout.alert_dialog, null)
+                AlertDialog.Builder(this@MainActivity)
+                    .setTitle(R.string.textUpperLimit)
+                    .setView(contentView)
+                    .setPositiveButton(android.R.string.ok) {_, _ ->
+                        val editText = contentView.findViewById<EditText>(R.id.upperLimitEditText)
+                        upperLimit = editText.text.toString().toInt()
+                    }
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .show()
             }
         }
     }
