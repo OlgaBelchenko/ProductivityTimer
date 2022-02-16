22621fb4411f50d2511c903e267361187fce68c3
Olga Belchenko <>
2022-02-16 16:18:34 +0500

    Added notification

--- app/src/main/java/ru/olgabelchenko/productivitytimer/MainActivity.kt
+++ app/src/main/java/ru/olgabelchenko/productivitytimer/MainActivity.kt
@@ -1,8 +1,14 @@
 package ru.olgabelchenko.productivitytimer
 
 import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,8 +16,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import ru.olgabelchenko.productivitytimer.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
+
+const val CHANNEL_ID = "org.hyperskill"
+const val CHANNEL_NAME = "Stopwatch"
+const val NOTIFICATION_ID = 393939
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        createNotificationChannel()
 
         with(binding) {
 
@@ -57,6 +70,9 @@ class MainActivity : AppCompatActivity() {
                 override fun run() {
                     if (upperLimit != 0 && time > upperLimit) {
                         textView.setTextColor(Color.RED)
+                    }
+                    if (upperLimit != 0 && time == upperLimit + 1) {
+                        showNotification()
                     }
                     handler.postDelayed(this, 1000)
                 }
@@ -98,5 +114,40 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
         }
+    }
+
+
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH).apply {
+                    lightColor = Color.RED
+                enableLights(true)
+            }
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotification() {
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Notification")
+            .setContentText("Time exceeded")
+            .setStyle(NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
     }
 }
